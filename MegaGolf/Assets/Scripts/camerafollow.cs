cd23b65174f0577ba90367a8a5cf3df37c481cf4
ctrlcookie <72091627+ctrlcookie@users.noreply.github.com>
2021-02-16 17:05:08 +0000

    camerafollow script

---
+++ MegaGolf/Assets/Scripts/camerafollow.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class camerafollow : MonoBehaviour
+{
+    public Transform Target;
+
+    private float speed = 10f;
+    public Vector3 Offset;
+
+    private void FixedUpdate()
+    {
+        Vector3 despos = Target.position + Offset;
+        Vector3 smoPos = Vector3.Lerp(a: transform.position, b: despos, t: speed * Time.deltaTime);
+        transform.position = despos;
+
+        transform.LookAt(Target);
+    }
+}
